a32d9fc9edafb3187134d9ffdf55e687da07ed85
oscar <oscar@ercilla.com>
2025-10-03 16:01:25 +0200

    Crear la clase boligrafo

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -7,8 +7,8 @@ public class Main {
         //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
         // to see how IntelliJ IDEA suggests fixing it.
         System.out.printf("Hello and welcome!");
-
-        for (int i = 1; i <= 5; i++) {
+        System.out.printf("clase del viernes");;
+        for (int i = 1; i <= 15; i++) {
             //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
             // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
             System.out.println("i = " + i);
---
+++ src/main/java/org/example/Models/Boligrafo.java
@@ -0,0 +1,37 @@
+package org.example.Models;
+
+public class Boligrafo {
+    private String codigo;
+    private String color;
+    private String marca;
+
+    public Boligrafo(String codigo, String color, String marca) {
+        this.codigo = codigo;
+        this.color = color;
+        this.marca = marca;
+    }
+
+    public String getCodigo() {
+        return codigo;
+    }
+
+    public void setCodigo(String codigo) {
+        this.codigo = codigo;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public String getMarca() {
+        return marca;
+    }
+
+    public void setMarca(String marca) {
+        this.marca = marca;
+    }
+}
